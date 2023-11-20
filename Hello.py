9a61037d81b0dd507934806f691e9edcb45a9244
Matheus Oliver de Carvalho Cerqueira <escrevaparamatheus@gmail.com>
2023-11-20 00:25:44 +0000

    inicio

--- Hello.py
+++ Hello.py
@@ -1,51 +1,21 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
 def run():
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
+        initial_sidebar_state="collapsed" 
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    st.write("# Listinha de Matheus e Brenda")
+    tab1, tab2 = st.tabs(['A Comprar','Já Temos'])
+    with tab1:
+      st.warning('[Nintendo Switch - 2000](https://www.amazon.com.br/Nintendo-Switch-Model-Legend-Kingdom/dp/B0BZK5M9TD/ref=asc_df_B0BZK5M9TD/?tag=googleshopp00-20&linkCode=df0&hvadid=648484803043&hvpos=&hvnetw=g&hvrand=17180737092398948441&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001533&hvtargid=pla-2187569339971&mcid=cd361c07374e3517a6961f1980b8e0c4&th=1)')
+    with tab2:
+      st.success('b')
 
 if __name__ == "__main__":
     run()
